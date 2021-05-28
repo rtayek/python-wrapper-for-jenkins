c5ab3678165f0f3e1cebbddee0ab59e9c53bcf87
rtayek <rtayek@gmail.com>
2021-05-27 19:44:42 -0700

    cleaned up.

--- jen.py
+++ jen.py
@@ -1,10 +1,8 @@
 #https://python-jenkins.readthedocs.io/en/latest/index.html
 #pip install python-jenkin
-print("start")
-import jenkins
 import sys
+import jenkins
 def getServer(address="192.168.1.6",port='8081',timeout=5,user="admin",password="admin"):
-    print("user and password:",user,password)
     server=jenkins.Jenkins('http://'+address+':'+port, username=user, password=password)
     if not server.wait_for_normal_op(30): print("Jenkins failed to be ready in sufficient time"); exit(2)
     return server
@@ -21,14 +19,9 @@ def getInfo(server):
     #print(len(plugins),'plugins:',names(plugins,name='shortName'))
     return jobs
 def main():
-    print("in main")
-    if len(sys.argv)<2:
-        raise Exception("user and password please")
-    print("arguments:",sys.argv)
+    if len(sys.argv)<2: raise Exception("user and password please")
     server=getServer(user=sys.argv[1],password=sys.argv[2])
     jobs=getInfo(server)
     try: server.create_node('foo')
     except Exception as e: print(str(e)[:80])
-if __name__ == "__main__":
-    print("main")
-    main()
+if __name__ == "__main__": main()
